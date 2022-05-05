b2453d813d3da31568e7d7237a73e9303fc8ce26
Aaron Au <aaron.au@me.com>
2022-05-05 16:01:27 -0600

    Fix bugs in get all reservations function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -86,12 +86,12 @@ exports.addUser = addUser;
 const getAllReservations = function(guest_id, limit = 10) {
   return pool
     .query(`
-      SELECT properties.* reservations.* 
+      SELECT properties.*, reservations.*, avg(rating) as average_rating 
       FROM properties
       JOIN reservations ON properties.id = reservations.property_id
       JOIN property_reviews ON properties.id = property_reviews.property_id
       WHERE reservations.guest_id = $1
-      GROUP BY reservations.id, properties.title, properties.cost_per_night, reservations.start_date
+      GROUP BY properties.id, reservations.id
       ORDER BY reservations.start_date
       LIMIT $2;`,
       [guest_id, limit])
